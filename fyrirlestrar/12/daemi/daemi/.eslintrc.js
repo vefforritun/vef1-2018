803d4a6acdd472a595b3e9b0a6808f50a7ce3b6b
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2018-11-18 21:56:59 +0000

    Laga lint villur

--- fyrirlestrar/12/daemi/daemi/.eslintrc.js
+++ fyrirlestrar/12/daemi/daemi/.eslintrc.js
@@ -6,6 +6,9 @@ module.exports = {
   rules: {
     'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
     'class-methods-use-this': 0,
+
+    // höfum ekki node_modules installað fyrir dæmi
+    'import/no-unresolved': 0,
   },
   plugins: ['import'],
 };
---
+++ fyrirlestrar/12/daemi/tools/rollup-plugins/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  rules: {
+    // höfum ekki node_modules installað fyrir dæmi
+    'import/no-unresolved': 0,
+  },
+};
